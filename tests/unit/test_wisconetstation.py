bd2b09b004210fc203ff2010e82d922bfef95c1c
ilmcconnell <iain.mcconnell@gmail.com>
2025-04-08 08:30:56 -0500

    Ignore no data stations, test closest station fns

--- tests/unit/test_wisconetstation.py
+++ tests/unit/test_wisconetstation.py
@@ -12,11 +12,11 @@ from utilities import within
         ("Verona", 43.0747, -89.3841, 13_580),
     ]
 )
-def test_wisconetstation_distance(station_name, lat, lon, expected_distance):
+def test_wisconetstation_distance_to_point(station_name, lat, lon, expected_distance):
     stations = all_stations()
     specific_station = [s for s in stations if s.station_name.lower() == station_name.lower()][0]
     s = WisconetStation(specific_station)
-    distance = s.distance(lat, lon)
+    distance = s.distance_to_point(lat, lon)
     assert isinstance(distance, float), "Expected distance to be a float"
     assert distance >= 0, "Expected distance to be non-negative"
     assert within(distance, expected_distance, 50), f"Expected delta distance to be less then 50"
